25b572a7812ab887be98c1d14c9172f94d1a58ce
Ansel001 <stefano.scarrone@anseltechnology.it>
2020-12-15 12:22:29 +0100

    added isConnected, connect, disconnect, getTag methods

--- src/android/LeanConnectPlugin.java
+++ src/android/LeanConnectPlugin.java
@@ -13,25 +13,74 @@ import hsc.com.leanconnectlibforservices.LeanConnectMobile;
 /**
  * This class echoes a string called from JavaScript.
  */
-public class LeanConnectPlugin extends CordovaPlugin {
+public class LeanConnectPlugin 
+        extends CordovaPlugin 
+        implements LeanConnectInterface.OnCommandResponseListener, LeanConnectInterface.OnConnectionListener{
 
+
+    private static final String IS_CONNECTED = "isConnected";
+    private static final String CONNECT = "connect";
+    private static final String DISCONNECT = "disconnect";
+    private static final String GET_TAG = "getTag";
     private LeanConnectInterface leanConnectInterface;
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("coolMethod")) {
-            String message = args.getString(0);
-            this.coolMethod(message, callbackContext);
+        this.leanConnectInterface = new LeanConnectMobile(this);
+        if (action.equals(IS_CONNECTED)) {
+            this.isConnected(callbackContext);
             return true;
+        } else if (action.equals(CONNECT)) {
+            this.connect(callbackContext);
+            return true;
+        } else if (action.equals(DISCONNECT)) {
+            this.disconnect(callbackContext);
+            return true;
+        } else if (action.equals(GET_TAG)) {
+            this.getTag(args, callbackContext);
         }
         return false;
     }
 
-    private void coolMethod(String message, CallbackContext callbackContext) {
-        if (message != null && message.length() > 0) {
-            callbackContext.success(message);
-        } else {
-            callbackContext.error("Expected one non-empty string argument.");
+    private void isConnected(CallbackContext callbackContext) {
+        try {
+            boolean res = leanConnectInterface.isConnected();
+            callbackContext.success(res);
+        } catch (Exception e) {
+            e.printStackTrace();
+            callbackContext.error(e.getMessage());
+        }
+    }
+
+    private void connect(CallbackContext callbackContext) {
+        try {
+            boolean res = leanConnectInterface.connect();
+            callbackContext.success(res);
+        } catch (Exception e) {
+            e.printStackTrace();
+            callbackContext.error(e.getMessage());
+        }
+    }
+
+    private void disconnect(CallbackContext callbackContext) {
+        try {
+            leanConnectInterface.disconnect();
+            callbackContext.success();
+        } catch (Exception e) {
+            e.printStackTrace();
+            callbackContext.error(e.getMessage());
+        }
+    }
+
+    private void getTag(JSONArray args, CallbackContext callbackContext) {
+        String var1 = "";
+        try {
+            var1 = args.getString(0);
+            leanConnectInterface.getTag(var1);
+            callbackContext.success();
+        } catch (Exception e) {
+            e.printStackTrace();
+            callbackContext.error("Tag is undefined");
         }
     }
 }
